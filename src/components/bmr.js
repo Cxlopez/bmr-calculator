a8aca9cd21d356937326367d2fbe94762fd23c33
Cxlopez <cristian4402@hotmail.com>
2023-04-23 13:06:02 -0400

    create bmr component

--- src/components/bmr.js
+++ src/components/bmr.js
@@ -2,7 +2,33 @@ import React from 'react'
 
 function bmr() {
   return (
-    <div>bmr</div>
+    <div>
+      <div className='container'>
+        <h2 className='center'>BMR Calculator</h2>
+      </div>
+      <from>
+        <div>
+          <label>Weight (lbs)</label>
+          <input  value={weight}/>
+        </div>
+        <div>
+          <label>Height (cm)</label>
+          <input  value={height}/>
+        </div>
+        <div>
+          <label>Age</label>
+          <input  value={age}/>
+        </div>
+      </from>
+      <div className='center'>
+        <h3>Your BMR is: {bmr}</h3>
+        <p>{message}</p>
+      </div>
+
+      <div className='img-container'>
+        <img src="{imgSrc}" alt="image" />
+      </div>
+    </div>
   )
 }
 
